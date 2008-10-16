44bebca2d21e7eee26c92b796e6b2aa84e2cc548
Matt Todd <chiology@gmail.com>
2008-10-16 01:10:36 -0400

    Removed unneeded dependencies.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -7,8 +7,8 @@ dependency "merb-slices", "0.9.9"        # Provides a mechanism for letting plug
 dependency "merb-auth", "0.9.9"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
  
 dependency "dm-core", "0.9.6"         # The datamapper ORM
-dependency "dm-aggregates", "0.9.6"   # Provides your DM models with count, sum, avg, min, max, etc.
+# dependency "dm-aggregates", "0.9.6"   # Provides your DM models with count, sum, avg, min, max, etc.
 dependency "dm-migrations", "0.9.6"   # Make incremental changes to your database.
 dependency "dm-timestamps", "0.9.6"   # Automatically populate created_at, created_on, etc. when those properties are present.
-dependency "dm-types", "0.9.6"        # Provides additional types, including csv, json, yaml.
+# dependency "dm-types", "0.9.6"        # Provides additional types, including csv, json, yaml.
 dependency "dm-validations", "0.9.6"  # Validation framework
