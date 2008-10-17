25f51188d6ccc4b3a18e52edb56cdfefc35d7054
Matt Todd <chiology@gmail.com>
2008-10-17 03:45:37 -0400

    Added auth columns migration.

---
+++ schema/migrations/004_add_auth_columns_to_users_migration.rb
@@ -0,0 +1,15 @@
+migration 4, :add_auth_columns_to_users  do
+  up do
+    modify_table :users do
+      add_column :crypted_password, String
+      add_column :salt, String
+    end
+  end
+  
+  down do
+    modify_table :users do
+      drop_column :crypted_password
+      drop_column :salt
+    end
+  end
+end
